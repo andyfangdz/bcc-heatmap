a934ce0b2e4ca79692dd45e45ab6f245ea1ff50a
Andy Fang <andyfang.dz@gmail.com>
2018-04-12 16:55:20 -0400

    add heatmap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import { Button, Intent, Spinner } from '@blueprintjs/core';
+import HeatMap from './components/HeatMap';
 
 import './styles';
 
 class App extends Component {
   render() {
-    return <Spinner intent={Intent.PRIMARY} />;
+    return (
+      <div>
+        <HeatMap
+          width={800}
+          height={500}
+          events={true}
+          margin={{
+            top: 10,
+            left: 40,
+            right: 30,
+            bottom: 80,
+          }}
+        />
+      </div>
+    );
   }
 }
 
--- frontend/src/styles/index.js
+++ frontend/src/styles/index.js
@@ -1,3 +1,5 @@
 import 'normalize.css/normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+
+import './index.css';
