835ce6501b1a0316197709847513aeedb6366a0c
Andy Fang <andyfang.dz@gmail.com>
2018-04-12 16:05:40 -0400

    boilerplate

---
+++ frontend/devserver.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const proxy = require('http-proxy-middleware');
+const Bundler = require('parcel-bundler');
+const express = require('express');
+
+const bundler = new Bundler('src/index.html');
+const app = express();
+
+const appPort = Number(process.env.PORT || 1234);
+
+console.log(`Dev Server Starting at http://localhost:${appPort}`);
+
+app.use(
+    '/api',
+    proxy({
+        target: 'http://localhost:4567',
+    })
+);
+
+app.use(bundler.middleware());
+
+app.listen(appPort);
